a41a77cf7d0e79d29adea6309746ab3b44f55b7f
LAPTOP-107LUOO6\developer <397590052@qq.com>
2020-02-21 23:37:49 +0800

    添加list文件 just 初始化 github不断 马上过12点了

--- src/pages/city/city.vue
+++ src/pages/city/city.vue
@@ -6,17 +6,20 @@
   <div>  
     <city-header></city-header>
     <Search></Search>
+    <list></list>
   </div>
 </template>
 
 <script>
 import CityHeader from "./components/header"
 import Search from "./components/search"
+import list from "./components/list"
 export default {
   name: 'City',
     components:{
       CityHeader,
-      Search
+      Search,
+      list
     }
 }
 </script>
---
+++ src/pages/city/components/list.vue
@@ -0,0 +1,13 @@
+<style lang="stylus" scoped>
+
+</style>
+
+<template>
+  <div class="list">list</div>
+</template>
+
+<script>
+export default {
+  name: 'CityList'
+}
+</script>
